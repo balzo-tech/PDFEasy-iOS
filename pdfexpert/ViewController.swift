9ce3b8b489a74912269abe711c1bc84deb9aa4c0
giuslape <giuseppe@balzo.eu>
2022-12-14 17:52:37 +0100

    Stack view with equispacing Label

--- pdfexpert/ViewController.swift
+++ pdfexpert/ViewController.swift
@@ -16,31 +16,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         
+        let spacing:CGFloat = 20
+        
         super.viewDidLoad()
-
         self.view.backgroundColor = ColorPalette.color(withType: .secondary)
                 
-        self.scrollStackView = ScrollStackView(axis: .vertical, horizontalInset: Constants.Style.DefaultHorizontalMargins)
-        self.view.addSubview(scrollStackView)
-        self.scrollStackView.stackView.backgroundColor = .red
-        self.scrollStackView.autoPinEdgesToSuperviewSafeArea(with: UIEdgeInsets(top: 44, left: 0, bottom: 24, right: 0))
-        self.scrollStackView.stackView.spacing = 30
+        let stackView = UIStackView.create(withAxis: .vertical)
+        self.view.addSubview(stackView)
+        stackView.backgroundColor = .red
+        stackView.distribution = .equalSpacing
+        stackView.autoPinEdgesToSuperviewSafeArea(with: .zero)
+        stackView.spacing = 30
+
         
-        self.scrollStackView.stackView.addBlankSpace(space: 40)
-        scrollStackView.stackView.addLabel(withText: "Bottone 1",
-                                           fontStyle: .title,
-                                           colorType: .primaryText)
-        self.scrollStackView.stackView.addBlankSpace(space: 40)
+        stackView.addBlankSpace(space: spacing)
 
-        scrollStackView.stackView.addLabel(withText: "Bottone 2",
+        stackView.addLabel(withText: "Bottone 1",
                                            fontStyle: .title,
                                            colorType: .primaryText)
-        self.scrollStackView.stackView.addBlankSpace(space: 40)
+        stackView.addBlankSpace(space: spacing)
 
-        scrollStackView.stackView.addLabel(withText: "Bottone 3",
+        stackView.addLabel(withText: "Bottone 2",
                                            fontStyle: .title,
                                            colorType: .primaryText)
-        self.scrollStackView.stackView.addBlankSpace(space: 40)
+        stackView.addBlankSpace(space: spacing)
+
+        stackView.addLabel(withText: "Bottone 3",
+                                           fontStyle: .title,
+                                           colorType: .primaryText)
+        stackView.addBlankSpace(space: spacing)
         
     }
 }
