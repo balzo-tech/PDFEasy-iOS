c038a16c006b08739295d42b4ef64e28648967c1
giuslape <giuseppe@balzo.eu>
2022-12-19 12:10:48 +0100

    Change font palette

--- pdfexpert/UI/Style/FontPalette.swift
+++ pdfexpert/UI/Style/FontPalette.swift
@@ -27,14 +27,16 @@ public enum FontStyle: String, CaseIterable {
     case header3
     case paragraph
     case menu
+    case titleButton
     
     fileprivate var defaultData: FontStyleData {
         switch self {
-        case .title: return FontStyleData(font: UIFont.systemFont(ofSize: 24.0, weight: .regular), lineSpacing: 6.0, uppercase: false)
-        case .header2: return FontStyleData(font: UIFont.systemFont(ofSize: 20.0, weight: .regular), lineSpacing: 6.0, uppercase: false)
-        case .paragraph: return FontStyleData(font: UIFont.systemFont(ofSize: 16.0, weight: .regular), lineSpacing: 5.0, uppercase: false)
-        case .header3: return FontStyleData(font: UIFont.systemFont(ofSize: 13.0, weight: .regular), lineSpacing: 3.0, uppercase: false)
-        case .menu: return FontStyleData(font: UIFont.systemFont(ofSize: 13.0, weight: .regular), lineSpacing: 3.0, uppercase: true)
+        case .title: return FontStyleData(font: UIFont(name: "Montserrat-ExtraBold", size: 28.0) ?? UIFont.systemFont(ofSize: 24.0, weight: .bold), lineSpacing: 6.0, uppercase: false)
+        case .header2: return FontStyleData(font: UIFont(name: "Montserrat-Regular", size: 20.0) ?? UIFont.systemFont(ofSize: 20.0, weight: .regular), lineSpacing: 6.0, uppercase: false)
+        case .paragraph: return FontStyleData(font: UIFont(name: "Montserrat-Medium", size: 16.0) ?? UIFont.systemFont(ofSize: 16.0, weight: .regular), lineSpacing: 5.0, uppercase: false)
+        case .header3: return FontStyleData(font: UIFont(name: "Montserrat-Medium", size: 13.0) ?? UIFont.systemFont(ofSize: 13.0, weight: .regular), lineSpacing: 5.0, uppercase: false)
+        case .menu: return FontStyleData(font: UIFont(name: "Montserrat-Medium", size: 13.0) ?? UIFont.systemFont(ofSize: 13.0, weight: .regular), lineSpacing: 5.0, uppercase: false)
+        case .titleButton: return FontStyleData(font: UIFont(name: "Montserrat-Bold", size: 16.0) ?? UIFont.systemFont(ofSize: 16.0, weight: .bold), lineSpacing: 5.0, uppercase: false)
         }
     }
 }
