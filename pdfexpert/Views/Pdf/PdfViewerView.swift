58a06777cf66b716974b0dfe9b49484f28f0945d
Leonardo Passeri <leonardo@balzo.eu>
2023-04-11 17:50:13 +0200

    Added viewer in pdf edit flow

--- pdfexpert/Views/Pdf/PdfViewerView.swift
+++ pdfexpert/Views/Pdf/PdfViewerView.swift
@@ -7,28 +7,66 @@
 
 import SwiftUI
 import Factory
+import PDFKit
+
+struct PDFKitView: UIViewRepresentable {
+    typealias UIViewType = PDFView
+
+    let pdfDocument: PDFDocument?
+    let singlePage: Bool
+    let pageMargins: UIEdgeInsets?
+
+    init(pdfDocument: PDFDocument?,
+         singlePage: Bool = false,
+         pageMargins: UIEdgeInsets? = nil) {
+        self.pdfDocument = pdfDocument
+        self.singlePage = singlePage
+        self.pageMargins = pageMargins
+    }
+
+    func makeUIView(context: Context) -> UIViewType {
+        let pdfView = PDFView()
+        pdfView.document = self.pdfDocument
+        pdfView.autoScales = true
+        if self.singlePage {
+            pdfView.displayMode = .singlePage
+        }
+        if let pageMargins = self.pageMargins {
+            pdfView.pageBreakMargins = pageMargins
+        }
+        return pdfView
+    }
+
+    func updateUIView(_ pdfView: UIViewType, context: Context) {
+        pdfView.document = self.pdfDocument
+    }
+}
 
 struct PdfViewerView: View {
     
     @StateObject var pdfViewerViewModel: PdfViewerViewModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: { self.pdfViewerViewModel.share() }) {
-                        Image(systemName: "square.and.arrow.up")
-                            .foregroundColor(ColorPalette.primaryText)
-                    }
+        PDFKitView(
+            pdfDocument: self.pdfViewerViewModel.pdf.pdfDocument,
+            pageMargins: UIEdgeInsets(top: 0, left: 0, bottom: 24, right: 0)
+        )
+        .padding([.leading, .trailing], 16)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: { self.pdfViewerViewModel.share() }) {
+                    Image(systemName: "square.and.arrow.up")
+                        .foregroundColor(ColorPalette.primaryText)
                 }
             }
-            .fullScreenCover(isPresented: self.$pdfViewerViewModel.monetizationShow) {
-                SubscriptionView(onComplete: { self.pdfViewerViewModel.monetizationShow = false })
-            }
-            .sheet(item: self.$pdfViewerViewModel.pdfToBeShared) { pdf in
-                ActivityViewController(activityItems: [pdf.data!],
-                                       thumbnail: pdf.thumbnail)
-            }
+        }
+        .fullScreenCover(isPresented: self.$pdfViewerViewModel.monetizationShow) {
+            SubscriptionView(onComplete: { self.pdfViewerViewModel.monetizationShow = false })
+        }
+        .sheet(item: self.$pdfViewerViewModel.pdfToBeShared) { pdf in
+            ActivityViewController(activityItems: [pdf.data!],
+                                   thumbnail: pdf.thumbnail)
+        }
     }
 }
 
