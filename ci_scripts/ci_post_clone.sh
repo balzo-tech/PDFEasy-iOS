5545961350736764fbb83f673cdfee32966aeae7
Leonardo Passeri <leonardo@balzo.eu>
2023-07-21 14:49:29 +0200

    Incremented marketing version to 1.16

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -10,6 +10,8 @@ echo "POST CLONE-Xcode Build started"
 
 BasePath=${CI_WORKSPACE}/PdfExpert
 
+# GoogleService-Info.plist
+
 CurrentFilePath=${BasePath}/Resources/Staging/GoogleService-Info.plist
 touch "${CurrentFilePath}"
 echo $CUSTOM_GOOGLE_SERVICE_INFO >"${CurrentFilePath}"
@@ -18,6 +20,8 @@ CurrentFilePath=${BasePath}/Resources/Production/GoogleService-Info.plist
 touch "${CurrentFilePath}"
 echo $CUSTOM_GOOGLE_SERVICE_INFO >"${CurrentFilePath}"
 
+# Info.plist
+
 CurrentFilePath=${BasePath}/Resources/Staging/Info.plist
 touch "${CurrentFilePath}"
 echo $CUSTOM_GOOGLE_SERVICE_INFO >"${CurrentFilePath}"
@@ -25,6 +29,12 @@ echo $CUSTOM_GOOGLE_SERVICE_INFO >"${CurrentFilePath}"
 CurrentFilePath=${BasePath}/Resources/Production/Info.plist
 touch "${CurrentFilePath}"
 echo $CUSTOM_GOOGLE_SERVICE_INFO >"${CurrentFilePath}"
+
+# ProjectInfo.plist
+
+CurrentFilePath=${BasePath}/Resources/ProjectInfo.plist
+touch "${CurrentFilePath}"
+echo $CUSTOM_PROJECT_INFO >"${CurrentFilePath}"
 
 echo "POST CLONE-Xcode Build finished"
 
